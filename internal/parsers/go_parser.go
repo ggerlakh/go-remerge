ead0e71f3668820e0044bca461652a6eb1bcaec8
ggerlakh <ggerlah55@gmail.com>
2023-02-19 19:35:14 +0300

    added beta golang file_dependency

--- internal/parsers/go_parser.go
+++ internal/parsers/go_parser.go
@@ -1,5 +1,15 @@
 package parsers
 
+import (
+	"fmt"
+	"go-remerge/tools/ostool"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"path/filepath"
+	"regexp"
+)
+
 type GoParser struct{}
 
 func (parser *GoParser) ExtractInheritance(filepath, entityName string) []string {
@@ -7,8 +17,67 @@ func (parser *GoParser) ExtractInheritance(filepath, entityName string) []string
 	panic("implement me")
 }
 
-func (parser *GoParser) ExtractDependencies(nodeName string) []string {
-	return []string{}
+func (parser *GoParser) ExtractDependencies(filePath string) []string {
+	var fileResults []string
+	var fileDependenciesMap = make(map[string]struct{})
+	// Specify the path of the Go file to analyze
+	//filePath := "./go-remerge/internal/analyzer/analyzer.go"
+	// Parse the file
+	fset := token.NewFileSet()
+	node, err := parser.ParseFile(fset, filePath, nil, parser.ParseComments)
+	if err != nil {
+		panic(err)
+	}
+
+	// Iterate through the imports and extract the structure objects
+	for _, i := range node.Imports {
+		// Get the import path
+		path := i.Path.Value[1 : len(i.Path.Value)-1]
+
+		// Parse the imported file
+		if !ostool.Exists(path) {
+			fmt.Printf("Skipping external dependency %v\n", path)
+			fileDependenciesMap["external_dependency/"+path] = struct{}{}
+			continue
+		}
+		packageGoFiles, err := filepath.Glob(filepath.Join(path, "*.go"))
+		if err != nil {
+			panic(err)
+		}
+		for _, packageGoFile := range packageGoFiles {
+			importedNode, err := parser.ParseFile(fset, packageGoFile, nil, parser.ParseComments)
+			if err != nil {
+				panic(err)
+			}
+
+			// Iterate through the top-level declarations and find the structures
+			for _, decl := range importedNode.Decls {
+				switch decl.(type) {
+				case *ast.GenDecl:
+					genDecl := decl.(*ast.GenDecl)
+					if genDecl.Tok == token.TYPE {
+						for _, spec := range genDecl.Specs {
+							typeSpec := spec.(*ast.TypeSpec)
+							lines := ostool.FilterComments(filePath)
+							regex := `^.*` + filepath.Base(path) + `\.` + typeSpec.Name.Name + `.*$`
+							importedStruct := regexp.MustCompile(regex)
+							for _, line := range lines {
+								if importedStruct.MatchString(line) {
+									fileDependenciesMap[packageGoFile] = struct{}{}
+									//fmt.Printf("Structure: %s, File: %s\n", typeSpec.Name, packageGoFile)
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	for file, _ := range fileDependenciesMap {
+		fileResults = append(fileResults, file)
+	}
+	//fmt.Println(fileResults)
+	return fileResults
 }
 
 func (parser *GoParser) ExtractEntities(filepath string) []string {
--- tools/ostool/file.go
+++ tools/ostool/file.go
@@ -1,7 +1,9 @@
 package ostool
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
 	"log"
 	"os"
 )
@@ -16,6 +18,57 @@ func IsDir(path string) (bool, error) {
 	} else {
 		return false, nil
 	}
+}
+
+func FilterComments(filename string) []string {
+	var linesWithoutComments []string
+	// Open file for reading
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	// Read file line by line and filter out comments
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if !isComment(line) {
+			linesWithoutComments = append(linesWithoutComments, line)
+			//fmt.Println(line)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return linesWithoutComments
+}
+
+func isComment(line string) bool {
+	line = stripWhitespace(line)
+	if len(line) == 0 {
+		return false
+	}
+
+	if line[0] == '/' && len(line) > 1 {
+		if line[1] == '/' || line[1] == '*' {
+			return true
+		}
+	}
+	return false
+}
+
+func stripWhitespace(line string) string {
+	var result []rune
+	for _, ch := range line {
+		if ch != ' ' && ch != '\t' {
+			result = append(result, ch)
+		}
+	}
+	return string(result)
 }
 
 func Exists(path string) bool {
